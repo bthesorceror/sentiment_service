bc0acd9972a02e615cda67865e3c06a514086a80
bthesorceror <bthesorceror@gmail.com>
2014-12-12 21:44:57 -0700

    Setup form processing

--- test/integration_test.js
+++ test/integration_test.js
@@ -1,36 +1,75 @@
-var test = require('tape');
-var hyperquest = require('hyperquest');
-var Server = require('../lib/server');
+var qs         = require("querystring");
+var test       = require("tape");
+var hyperquest = require("hyperquest");
+var Server     = require("../lib/server");
 
-test('integration test', function(t) {
+test("integration tests", function(t) {
   var server = new Server(9999);
   server.start();
 
-  t.plan(3);
+  t.test("GET not allowed", function(t) {
+    t.plan(1);
 
-  var request = hyperquest.get("http://localhost:9999/");
+    var request = hyperquest.get("http://localhost:9999/");
 
-  request.on('response', function(res) {
-    t.ok(true, 'Response received');
-
-    var content_type = res.headers["content-type"];
-    t.equal(content_type, "application/json", "returns correct content-type");
-
-    var body = "";
-    res.on('data', function(data) {
-      body += data.toString();
-    });
-
-    res.on('end', function() {
-      t.deepEqual(
-        JSON.parse(body),
-        {name: "Brandon Farmer"},
-        "returns correct response"
-        );
+    request.on("response", function(res) {
+      t.equal(res.statusCode, 405, "returns correct status");
     });
   });
 
-  t.on('end', function() {
+  t.test("POST with proper parameters", function(t) {
+    t.plan(1);
+
+    var formData = qs.stringify({
+      text: "This is a very very bad post."
+    });
+
+    var headers = {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': formData.length
+    };
+
+    var request = hyperquest.post("http://localhost:9999/", {
+      headers: headers
+    });
+
+    request.on("response", function(res) {
+      t.equal(res.statusCode, 200, "returns correct status");
+    });
+
+    request.end(formData);
+  });
+
+  t.test("POST with no text", function(t) {
+    t.plan(2);
+
+    var headers = {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': 0
+    };
+
+    var request = hyperquest.post("http://localhost:9999/", {
+      headers: headers
+    });
+
+    request.on("response", function(res) {
+      t.equal(res.statusCode, 422, "returns correct status");
+
+      var body = "";
+      res.on('data', function(data) {
+        body += data.toString();
+      });
+
+      res.on('end', function() {
+        var expected = {error: "Must provide text to analyze."};
+        t.deepEqual(JSON.parse(body), expected, "returns correct error");
+      });
+    });
+
+    request.end();
+  });
+
+  t.on("end", function() {
     server.stop();
   });
 });
